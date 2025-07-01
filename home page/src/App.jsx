4845b198532acda9734a6d7e74f6c4b0d4b7c59d
Meeth <meethukothari@gmail.com>
2025-07-01 18:38:40 +0530

    feat: added login page

--- home page/src/App.jsx
+++ home page/src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import HomePage from './Components/HomePage'
 import Dashboard from './Components/Dashboard'
+import LoginPage from './Components/LoginPage'
 
 function App() {
   return (
-    <div className=' mx-auto flex justify-center w-full max-w-[1440px]'>
-      <HomePage />
-      <Dashboard />
+    <div>
+      {/* <div className=' mx-auto flex justify-center w-full max-w-[1440px]'>
+        <HomePage />
+        <Dashboard />
+      </div> */}
+      <LoginPage />
     </div>
   )
 }
